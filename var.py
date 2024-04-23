faf5698513be6adbc8652a9a4087cc35e30c7604
hemasathwika <135261406+hemasathwika@users.noreply.github.com>
2024-04-24 00:53:44 +0530

    basics

---
+++ var.py
@@ -0,0 +1,17 @@
+#variables declaration 
+
+#way 1
+_myName = 'sathwika'
+print(_myName)
+
+#way 2
+myName = 'sathwika'
+print(myName)
+
+#way 3
+my_Name = 'sathwika'
+print(my_Name)
+
+#way 4
+my_Name1 = 'sathwika'
+print(my_Name1)
